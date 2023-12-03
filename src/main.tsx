40ddcaf8f16f62a2604afe3a95ebc3f0788a1535
Esraa Mohamed Abodeif <esraa.abodeif1@gmail.com>
2023-12-03 15:47:48 +0200

    chore: setup login + sign pages + router

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App";
+import SignUp from "./page/SignUp";
+import SignIn from "./page/SignIn";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/users/sign_in",
+    element: <SignIn />,
+  },
+  {
+    path: "/users/sign_up",
+    element: <SignUp />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
